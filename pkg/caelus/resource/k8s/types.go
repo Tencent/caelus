18f520acde458a98b104a2b60ea66241f4f3df7d
forrestchen <forrestchen@tencent.com>
2021-11-25 15:06:22 +0800

    uniform caelus resource name prefix
    Signed-off-by: forrestchen <forrestchen@tencent.com>

--- pkg/caelus/resource/k8s/types.go
+++ pkg/caelus/resource/k8s/types.go
@@ -19,13 +19,13 @@ import (
 	"fmt"
 	"strings"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	v1helper "k8s.io/kubernetes/pkg/apis/core/v1/helper"
 )
 
 const (
-	ExternalResourcePrefix = "mixer.io/ext-"
+	ExternalResourcePrefix = "mixer.kubernetes.io/ext-"
 )
 
 // NodeInfo group node level info
