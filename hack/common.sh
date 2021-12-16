f2304a0cdfb31cbec44cd9c7778dce686ebd5a08
forrestchen <forrestchen@tencent.com>
2021-12-16 17:32:44 +0800

    remove useless component metric_adapter
    Signed-off-by: forrestchen <forrestchen@tencent.com>

--- hack/common.sh
+++ hack/common.sh
@@ -7,7 +7,6 @@ PACKAGE_PREFIX="github.com/tencent"
 PACKAGE_NAME="caelus"
 PACKAGE="${PACKAGE_PREFIX}/${PACKAGE_NAME}"
 OUTPUT_PATH="${BASE_DIR}/_output"
-ADAPTER_NAME="caelus_metric_adapter"
 NM_OPERATOR_NAME="nm-operator"
 mkdir -p ${OUTPUT_PATH}
 USER_ID=$(id -u)
